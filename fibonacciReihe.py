e7c80d095822202ee495302dfec42eb443cad2ae
Recai Gül <recaiguel@gmail.com>
2026-03-25 12:09:22 +0100

    Code mit try except und Userinput dynamischer gemacht

--- fibonacciReihe.py
+++ fibonacciReihe.py
@@ -1,26 +1,29 @@
-""" # n definiert die häufigkeit der Folgen dieser Fibonacci-Reihe
-n = 15
+try:
+    # n definiert die häufigkeit der Folgen dieser Fibonacci-Reihe
+    n = int(input("Gib eine Zahl ein: "))
 
-# 0 und 1 ist der Startpunkt. Ohne diese beiden Werte 
-# funktioniert die Fibonacci Reihe nicht
-a = 0
-b = 1
+    # 0 und 1 ist der Startpunkt. Ohne diese beiden Werte 
+    # funktioniert die Fibonacci Reihe nicht
+    a = 0
+    b = 1
 
-for f in range(n):
+    for f in range(n):
 
-    # gibt die aktuelle Folge in der Fib.-Reihe wieder
-    print(a)    
+        # gibt die aktuelle Folge in der Fib.-Reihe wieder
+        print(a)    
 
-    # berechnet f als eine Folge in der Fibonacci-Reihe,
-    # der sich aus a + b ergibt
-    f = a + b
-    
-    # Überschreibt die Variablen so dass Sie auch die
-    # letzten beiden Werte in der Fib.-Reihe darstellen
-    a = b
-    b = f
- """    
+        # berechnet f als eine Folge in der Fibonacci-Reihe,
+        # der sich aus a + b ergibt
+        f = a + b
+        
+        # Überschreibt die Variablen so dass Sie auch die
+        # letzten beiden Werte in der Fib.-Reihe darstellen
+        a, b = b, f
+    else:
+        print("Die Fibonacci Folge beginnt immer mit '0, 1,'.")
 
+except ValueError:
+    print("bitte gib eine gültige Zahl ein.")
  
  
  
@@ -60,7 +63,7 @@ class FibonacciPython:
 
     fibonacci(6) """
 
-
+""" 
 class FibonacciPython:
 
     @staticmethod
@@ -76,4 +79,4 @@ class FibonacciPython:
 
         return b
         
-    print(fibonacci(9))    
+    print(fibonacci(9))    """ 
